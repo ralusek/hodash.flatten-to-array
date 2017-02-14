c5a18108fadd061c0156044ff542f7f90ebd372f
ralusek <ralusek@gmail.com>
2017-02-14 02:38:31 -0800

    added deep and formatted options.

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-'use strict';
+/**
+ *
+ */
+module.exports = function flattenToArray(iter, config) {
+  config = config || {};
 
+  const output = []
+  
+  // Explicitly set function rather than check conditional within for performance
+  // reasons.
+  const addToOutput = config.formatter ?
+    (item) => output.push(config.formatter(item)) :
+    (item) => output.push(item);
+
+  if (!config.deep) {
+    iter.forEach(item => {
+      // Duck typing for flatten. Does not confirm if function.
+      if (item.forEach) item.forEach(addToOutput);
+      else addToOutput(item);
+    });
+  }
+  else deepFlattenToArray(iter, addToOutput);
+  
+  return output;
+}
 
 /**
  *
  */
-module.exports = function(iter) {
-  const output = [];
+function deepFlattenToArray(iter, addToOutput) {
   iter.forEach(item => {
     // Duck typing for flatten. Does not confirm if function.
-    if (item.forEach) {
-      item.forEach(item => output.push(item));
-    }
-    else output.push(item);
+    if (item.forEach) deepFlattenToArray(item, addToOutput);
+    else addToOutput(item);
   });
-  return output;
-};
+}
